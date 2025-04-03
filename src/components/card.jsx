e395f0ab9f6cbccb4968c75a324ad53bcadab818
ARCG-prog <alejandro.rcg@gmail.com>
2025-04-03 10:38:26 -0600

    index

--- src/components/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children }) {
-  return <div className="bg-white shadow-md rounded-xl p-4">{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div className="p-2">{children}</div>;
-}
